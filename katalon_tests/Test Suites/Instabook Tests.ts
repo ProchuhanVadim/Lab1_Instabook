35774310c2efb3afd325023b4e59e09fc2294c4c
Olena Prochukhan <ironaxe2014@gmail.com>
2026-06-04 22:20:49 +0300

    Added Katalon Recorder login tests

---
+++ katalon_tests/Test Suites/Instabook Tests.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Instabook Tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ba26be7f-0f43-469a-9094-a8c7cd8be539</testSuiteGuid>
+   <testCaseLink>
+      <guid>dd524c57-2814-493d-99ff-8a4a11082bc5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Instabook Tests/User Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
